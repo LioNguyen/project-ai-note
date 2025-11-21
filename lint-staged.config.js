0525368b64dc7df42bac3d051a0f9afe2a16f988
Lio Nguyen <lio.nguyen.dev@gmail.com>
2025-11-21 10:18:02 +0700

    feat: add CI/CD

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,3 @@
-export default {
-  "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
-  "*.{md,json}": ["prettier --write"],
+module.exports = {
+  "*.{js,jsx,ts,tsx}": ["eslint --fix"],
 };
